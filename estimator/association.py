d7b8027f2445bc72b03f87361b8c739b120c1fdc
Patricio A. Vela <pvela@gatech.edu>
2023-12-14 12:05:38 -0500

    Adding data association

---
+++ estimator/association.py
@@ -0,0 +1,35 @@
+#=================================== association ===================================
+"""!
+@brief  Code base / API for performing association of multiple changing variables of
+        the same type, over time.  Essentially defines a consistent ID to each
+        variable.  
+
+There needs to exist some scheme for scoring these variables to preserve their ID.
+Attached to this implementation is a generic class for defining the variable
+features that will support association over "time."
+"""
+#=================================== association ===================================
+
+#
+# @file     association.py
+#
+# @author   Ruinian Xu          rxu@gatech.edu
+# @author   Patricio Vela       pvela@gatech.edu
+#
+# NOTE: 90 column view, wrap margin at 6
+#
+#=================================== association ===================================
+
+
+
+# ADD CODE. GET CORE FROM PUZZLE_SOLVER correspondences.
+# NEEDS TO HAVE PREDICT, MEASURE, CORRECT, ADAPT.
+# 
+# ADAPT SHOULD ALIGN VARIABLE AND FEATURE VECTOR AS BEST NEEDED.
+# CORRECT PERFORMS CORRECTION IN FEATURE SPACE.
+# I THINK ABOVE IS COMPATIBLE WITH HOW PUZZLE SOLVER DOES IT.
+#
+
+
+#
+#=================================== association ===================================
